2d52d0150f944ff71c22a4d944f498ca6f1d354d
Bereczki Lehel <151776956+L3H3L@users.noreply.github.com>
2024-06-08 01:12:23 +0200

    hamburger update

--- theme.js
+++ theme.js
@@ -1,9 +1,25 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const themeControllers = document.querySelectorAll('.theme-controller');
-
-    themeControllers.forEach(controller => {
-        controller.addEventListener('change', (event) => {
-            document.documentElement.setAttribute('data-theme', event.target.value);
-        });
+document.addEventListener("DOMContentLoaded", () => {
+    const themeDropdown = document.querySelectorAll('input[name="theme-dropdown"]');
+  
+    // Check if a theme is already saved in localStorage
+    const savedTheme = localStorage.getItem('selected-theme');
+    if (savedTheme) {
+      document.documentElement.setAttribute('data-theme', savedTheme);
+      themeDropdown.forEach((input) => {
+        if (input.value === savedTheme) {
+          input.checked = true;
+        }
+      });
+    }
+  
+    // Save the selected theme to localStorage
+    themeDropdown.forEach((input) => {
+      input.addEventListener('change', () => {
+        if (input.checked) {
+          document.documentElement.setAttribute('data-theme', input.value);
+          localStorage.setItem('selected-theme', input.value);
+        }
+      });
     });
-});
+  });
+  
